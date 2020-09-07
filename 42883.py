f0f409297e91bd7f3dfdf25e8a7ee95bef16337e
Woosung-korea <30863223+Woosung-korea@users.noreply.github.com>
2020-09-07 17:31:08 +0900

    Update 42883.py

--- 42883.py
+++ 42883.py
@@ -1,13 +1,22 @@
 def solution(number, k):
-    l = len(number)
+    l1 = len(number)
     numList, answer, answerl = [], '', 0
-    for i in range(l):
+    for i in range(l1):
         numList.append(int(number[i]))
-    l = l - k
-    while answerl < l:
-        idx = numList[:k + 1].index(max(numList[:k + 1]))
+    l2 = l1 - k
+    start = 0
+    while answerl < l2:
+        print('numList[start:start + k + 1]: %s' %numList[start:start + k + 1])
+        idx = start + numList[start:start + k + 1].index(max(numList[start:start + k + 1]))
+        start = idx + 1
         answer += str(numList[idx])
         answerl += 1
-        k -= idx
-        numList = numList[idx + 1:]
+        k -= idx - answerl
+        print('numList: %s, start: %s, idx: %s k: %s' % (numList, start, idx, k))
+        print('answer: %s' % answer)
+        print('----------------------------------')
+    print(answer)
+
     return answer
+
+solution("1231234", 3)
