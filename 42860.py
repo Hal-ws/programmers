1ffeb2a8dfb24bd99678802f85905b0a538eaf73
Woosung-korea <30863223+Woosung-korea@users.noreply.github.com>
2020-09-09 17:22:26 +0900

    Update 42860.py

--- 42860.py
+++ 42860.py
@@ -1,36 +1,46 @@
 def solution(name):
-    distance, answer, ln = [], 0, len(name)
-    leftflag = 0 # 한번이라도 왼쪽으로 움직이면 계속 왼쪽으로만 감
+    diff, answer, ln = [], 0, len(name)
+    jumpflag = 0 # 왼쪽 끝에서 점프했는지 확인
     for i in range(ln):
-        distance.append(ord(name[i]) - 65)
-        if distance[i] > 13:
-            distance[i] = 26 - distance[i]
-    maxcnt = ln - distance.count(0)
+        diff.append(ord(name[i]) - 65)
+        if diff[i] > 13:
+            diff[i] = 26 - diff[i]
+    maxcnt = ln - diff.count(0)
     i, idx = 0, 0
     while i < maxcnt:
-        answer += distance[idx]
-        if leftflag:
-            nextIdx = nextleft(distance, idx)
-            answer += (idx + ln - nextIdx)
-            idx = nextIdx
-        else: 
-
+        answer += diff[idx]
+        diff[idx] = 0
+        lDistance, rDistance = nextleft(diff, idx), nextright(diff, idx)
+        if rDistance < lDistance:
+            answer += rDistance
+            idx += rDistance
+        else:
+            jumpflag = 1 # 점프해준 후 다음 idx 지정, 다음 idx 까지 거리는 answer에 더해줌
+            answer += lDistance
+            idx = ln + idx - lDistance
+            break
         i += 1
-
-
-
-
-
-    print(distance)
+    if jumpflag:
+        std = idx
+        while idx >= 0:
+            if diff[idx] > 0:
+                answer += std - idx
+                answer += diff[idx]
+                diff[idx] = 0
+                std = idx
+            idx -= 1
     return answer
 
-def nextright(distance, idx):
-    l = len(distance)
-    for i in range(idx + 1, l):
-        if distance[i] != 0:
-            return i - idx ## 다음 문자까지의 거리
-    return -1 ## 끝까지 다봄
 
-def nextleft(distance, idx):
-    return idx
-solution("JEROEN")
+def nextright(diff, idx):
+    l = len(diff)
+    for i in range(idx + 1, l):
+        if diff[i] != 0:
+            return i - idx ## 다음 문자까지의 거리
+
+def nextleft(diff, idx):
+    l = len(diff)
+    for i in range(l - 1, -1, -1):
+        if diff[i] != 0:
+            return idx + l - i
+solution("JAN")
