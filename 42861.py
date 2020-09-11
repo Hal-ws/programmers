71583d7a998ae277742e34fb09b353355e137b2d
Woosung-korea <30863223+Woosung-korea@users.noreply.github.com>
2020-09-11 16:39:07 +0900

    Update 42861.py

--- 42861.py
+++ 42861.py
@@ -1,18 +1,29 @@
 def solution(n, costs):
     lc = len(costs)
     for i in range(lc):
-        costs[i][0], costs[i][1], costs[i][2] = costs[i][2], costs[i][0], costs[i][1]
+        if costs[i][0] < costs[i][1]:
+            costs[i][0], costs[i][1], costs[i][2] = costs[i][2], costs[i][0], costs[i][1]
+        else:
+            costs[i][0], costs[i][1], costs[i][2] = costs[i][2], costs[i][1], costs[i][0]
     cycle = []
     for i in range(n):
         cycle.append([i, i])
     costs.sort()
-    print(cycle)
-    print(costs)
-
     answer = 0
     return answer
+
 
 def getparent(idx, cycle):
     if cycle[idx][1] == cycle[idx][0]:
         return idx
     return getparent(cycle[idx][1], cycle)
+
+def unionparent(idx1, idx2, cycle):
+    a = getparent(idx1, cycle)
+    b = getparent(idx2, cycle)
+    if a == b: # 둘 다 root node가 같음
+        return;
+
+
+
+solution(5, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]])
