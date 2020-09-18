0ca5a6e388de9d4cc408eee3bc8f548748c9fcd7
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2020-09-19 02:33:35 +0900

    Update 42579.py

--- 42579.py
+++ 42579.py
@@ -3,49 +3,58 @@ def solution(genres, plays):
     for i in range(lp):
         plays[i] = [genres[i], plays[i], i]
     plays.sort()
-    classicSum = [[None, None]]
+    genreSum = [[None, None]]
     j = 0
     for i in range(lp):
-        if plays[i][0] != classicSum[j][1]:
-            classicSum.append([plays[i][1], plays[i][0]])
+        if plays[i][0] != genreSum[j][1]:
+            genreSum.append([plays[i][1], plays[i][0]])
             j += 1
         else:
-            classicSum[j][0] += plays[i][1]
-    classicSum = sorted(classicSum[1:len(classicSum)], reverse=True)
-    firstGenre, secondGenre = [], []
-    for i in range(lp):
-        if plays[i][0] == classicSum[0][1]:
-            firstGenre.append([plays[i][1], plays[i][2]])
-        if plays[i][0] == classicSum[1][1]:
-            secondGenre.append([plays[i][1], plays[i][2]])
-    firstGenre, secondGenre = sorted(firstGenre, reverse=True), sorted(secondGenre, reverse=True)
-    print('firstGenre: %s' %firstGenre)
-    print('secondGenre: %s' %secondGenre)
-    l1, l2, cnt, answer = len(firstGenre), len(secondGenre), 0, []
-    for i in range(1, l1):
-        if firstGenre[i][0] < firstGenre[i - 1][0]:
-            answer.append(firstGenre[i - 1][1])
-            cnt += 1
-        if cnt == 2:
-            break
-    if cnt == 0:
-        answer.append(firstGenre[l1 - 1][1])
-        answer.append(firstGenre[l1 - 2][1])
-    elif cnt == 1:
-        answer.append(firstGenre[l1 - 1][1])
-    cnt = 2
-    for i in range(1, l2):
-        if secondGenre[i][0] < secondGenre[i - 1][0]:
-            answer.append(secondGenre[i - 1][1])
-            cnt += 1
-        if cnt == 4:
-            break
-    if cnt == 2:
-        answer.append(secondGenre[l2 - 1][1])
-        answer.append(secondGenre[l2 - 2][1])
-    if cnt == 3:
-        answer.append(secondGenre[l2 - 1][1])
+            genreSum[j][0] += plays[i][1]
+    genreSum = sorted(genreSum[1:len(genreSum)], reverse=True)
+    print('genreSum: %s' %genreSum)
+    lg, answer = len(genreSum), []
+    for i in range(lg):
+        for j in range(lp):
+            if genreSum[i][1] == plays[j][0]:
+                start = j
+                break
+        for j in range(start, lp):
+            if plays[j][0] != plays[start][0]:
+                flag, end = 0, j
+                break
+            if j == lp - 1:
+                flag, end = 1, lp
+                break
+        answer += choicetwosongs(sorted(plays[start:end], reverse=True), end - start)
     print(answer)
     return answer
 
-solution(["classic", "pop", "classic", "classic", "pop"], [1000, 1000, 1000, 1000, 1000])
+
+def choicetwosongs(songs, l):
+    if l == 1:
+        return [songs[0][2]]
+    flag, flag2, temp = 0, 0, []
+    for i in range(1, l):
+        if songs[i][1] < songs[i - 1][1]:
+            flag = 1
+            start = i
+            break
+    if flag:
+        temp.append(songs[start - 1][2])
+        if start > 2:
+            temp.append(songs[start - 2][2])
+        else:
+            for j in range(start + 1, l):
+                if songs[j] < songs[j - 1]:
+                    flag2 = 1
+                    break
+            if flag2:
+                temp.append(songs[j - 1][2])
+            else:
+                temp.append(songs[l - 1][2])
+    else:
+        temp.append(songs[l - 1][2])
+        temp.append(songs[l - 2][2])
+    return temp
+solution(["classic", "classic", "classic", "classic", "pop"], [500, 150, 800, 800, 2500])
