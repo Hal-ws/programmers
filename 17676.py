27e2afea8a5a0fa0f0ae0e248e9a34979aaadc7c
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-23 13:52:19 +0900

    Update and rename 17676 to 17676.py

--- 17676.py
+++ 17676.py
@@ -7,9 +7,25 @@ def solution(lines):
         day, end, time = map(str, lines[i].split())
         timeTable.append([int(end[:2]), int(end[3:5]), int(end[6:8]), float('0' + end[8:12])])
         timeTable[i] = getsecondscale(timeTable[i], float(time[:len(time) - 1]))
+    timeTable = sorted(timeTable)
     for i in range(l):
         print(timeTable[i])
     answer = 0
+    for i in range(l):
+        firstProcessEnd = i
+        untilEndFlag = 1
+        for j in range(i + 1, l):
+            if round(timeTable[j][1] - timeTable[firstProcessEnd][0], 3) >= 1: ##
+                lastProcessStart = j - 1
+                untilEndFlag = 0
+                break
+        if untilEndFlag:
+            temp = l - firstProcessEnd
+        else:
+            temp = lastProcessStart - firstProcessEnd + 1
+        if answer <= temp:
+            answer = temp
+    print(answer)
     return answer
 
 
@@ -18,4 +34,8 @@ def getsecondscale(endTime, processingTime):
     temp = deepcopy(endTime)
     startTime = round(temp[0] * pow(60, 2) + temp[1] * pow(60, 1) + temp[2] + temp[3] - processingTime[0] - processingTime[1] + 0.001, 3)
     endTime = endTime[0] * pow(60, 2) + endTime[1] * pow(60, 1) + endTime[2] + endTime[3]
-    return [startTime, endTime]
+    return [endTime, startTime]
+
+
+
+solution(["2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"])
