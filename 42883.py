1ec8a89460684096b1fc78fe95c50eb9c1923a05
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-27 02:56:19 +0900

    Update 42883.py

--- 42883.py
+++ 42883.py
@@ -1,22 +1,23 @@
 def solution(number, k):
-    l1 = len(number)
-    numList, answer, answerl = [], '', 0
-    for i in range(l1):
-        numList.append(int(number[i]))
-    l2 = l1 - k
-    start = 0
-    while answerl < l2:
-        print('numList[start:start + k + 1]: %s' %numList[start:start + k + 1])
-        idx = start + numList[start:start + k + 1].index(max(numList[start:start + k + 1]))
-        start = idx + 1
-        answer += str(numList[idx])
-        answerl += 1
-        k -= idx - answerl
-        print('numList: %s, start: %s, idx: %s k: %s' % (numList, start, idx, k))
-        print('answer: %s' % answer)
-        print('----------------------------------')
-    print(answer)
-
+    answer = ''
+    stack = []
+    cnt = 0
+    for n in number:
+        num = int(n)
+        if cnt == k: # 더이상 못지움
+            stack.append(num)
+        else: # 지울 수 있음
+            if len(stack) == 0:
+                stack.append(num)
+            else:
+                while stack[-1] < num:
+                    cnt += 1
+                    stack.pop()
+                    if cnt == k or len(stack) == 0:
+                        break
+                stack.append(num)
+    for _ in range(k - cnt):
+        stack.pop()
+    for n in stack:
+        answer += str(n)
     return answer
-
-solution("1231234", 3)
