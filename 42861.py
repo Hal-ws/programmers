ccfa883af76ad1be09b4410d7814a1a4ab59fb76
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-03-27 23:14:07 +0900

    Update 42861.py

--- 42861.py
+++ 42861.py
@@ -1,29 +1,29 @@
 def solution(n, costs):
-    lc = len(costs)
-    for i in range(lc):
-        if costs[i][0] < costs[i][1]:
-            costs[i][0], costs[i][1], costs[i][2] = costs[i][2], costs[i][0], costs[i][1]
-        else:
-            costs[i][0], costs[i][1], costs[i][2] = costs[i][2], costs[i][1], costs[i][0]
-    cycle = []
-    for i in range(n):
-        cycle.append([i, i])
-    costs.sort()
+    global roots
     answer = 0
+    roots = [i for i in range(n)]
+    for i in range(len(costs)):
+        costs[i][0], costs[i][2] = costs[i][2], costs[i][0]
+    costs.sort()
+    for path in costs:
+        dis, node1, node2 = path[0], path[1], path[2]
+        root1, root2 = find(node1), find(node2)
+        if root1 != root2:
+            union(root1, root2)
+            answer += dis
     return answer
 
 
-def getparent(idx, cycle):
-    if cycle[idx][1] == cycle[idx][0]:
-        return idx
-    return getparent(cycle[idx][1], cycle)
-
-def unionparent(idx1, idx2, cycle):
-    a = getparent(idx1, cycle)
-    b = getparent(idx2, cycle)
-    if a == b: # 둘 다 root node가 같음
-        return;
+def union(node1, node2):
+    global roots
+    root1, root2 = find(node1), find(node2)
+    if root1 != root2:
+        roots[root2] = root1
 
 
-
-solution(5, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]])
+def find(node):
+    global roots
+    if roots[node] == node:
+        return node
+    roots[node] = find(roots[node])
+    return roots[node]
