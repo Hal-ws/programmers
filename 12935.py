d3900a1b44a8d69b91c83fe1574131982229ac83
Woosung Jeong <30863223+Woosung-korea@users.noreply.github.com>
2021-09-02 11:35:13 +0900

    Update 12935.py

--- 12935.py
+++ 12935.py
@@ -1,9 +1,5 @@
 def solution(arr):
-    minVal = None
-    for num in arr:
-        if minVal == None or num < minVal:
-            minVal = num
-    del arr[arr.index(minVal)]
+    del arr[arr.index(min(arr))]
     if len(arr) == 0:
         arr = [-1]
     return arr
